17eec515df082977939616da82294460a12301de
dentalaidestek <dentalaidestek@gmail.com>
2026-09-12 18:20:55 +0300

    Pin verified TVEM revisions and SHA256 hashes

--- vision_service/model_sources.py
+++ vision_service/model_sources.py
@@ -73,6 +73,7 @@ OPTIONAL_MODEL_SOURCES = {
         filename="Teeth_Visual_Experts_Maskdino_Swinl_x-ray_11diseases.pth",
         local_name="tvem_11diseases.pth",
         purpose="TVEM 11-disease specialist; deep caries/residual root/pontic",
+        revision="29081baac6b09aab4214477e72e80b25ce0a3eef",
     ),
     "tvem_bone_loss": ModelSource(
         key="tvem_bone_loss",
@@ -81,6 +82,8 @@ OPTIONAL_MODEL_SOURCES = {
         filename="Teeth_Visual_Experts_Maskdino_Swinl_x-ray_bone_loss_1disease.pth",
         local_name="tvem_bone_loss.pth",
         purpose="TVEM generic bone-loss helper",
+        revision="a3e5b74ceb23fd947e9b27dfc3980c22fbf33e27",
+        sha256="faea0004a39ef2420369066c546df7a29e2e1906c0247ccb45da44eafd629ddc",
     ),
     "tvem_canal_sinus": ModelSource(
         key="tvem_canal_sinus",
@@ -89,6 +92,8 @@ OPTIONAL_MODEL_SOURCES = {
         filename="Teeth_Visual_Experts_Maskdino_Swinl_panoramic_x-ray_Mandibular_Canal_Maxillary_Sinus.pth",
         local_name="tvem_mandibular_maxillary.pth",
         purpose="TVEM mandibular-canal/maxillary-sinus anatomy helper",
+        revision="0c7dd7ef2404da22889210be9e57a69f0d285052",
+        sha256="5e4678afd21c7aa5675982227cc7826b220eefbb98b0dc774dd20077b9d41ad6",
     ),
     "tvem_periapical3": ModelSource(
         key="tvem_periapical3",
@@ -97,6 +102,8 @@ OPTIONAL_MODEL_SOURCES = {
         filename="Teeth_Visual_Experts_DINO_r50_5scale_x-ray_periapical_lesions_3classes.pth",
         local_name="tvem_periapical3.pth",
         purpose="periapical lesion subtype helper",
+        revision="48be5960dafe950c498fba9075b627fa95acba3f",
+        sha256="5bd28f3ab57815c74b52176b0a2b4fa6d147d602f8b057ad12f19585814fc8d0",
     ),
 }
 
